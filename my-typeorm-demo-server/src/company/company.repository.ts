135c318eb8f08c04b5bc745dedf0b53bcc4f29d3
Ferenc Toth <toth_ferenc@hotmail.com>
2022-02-23 09:50:22 +0100

    removing my-typeorm-demo-server

--- my-typeorm-demo-server/src/company/company.repository.ts
+++
@@ -1,9 +0,0 @@
-import { EntityRepository, Repository } from 'typeorm';
-import { Company } from './company.entity';
-
-@EntityRepository(Company)
-export class CompanyRepository extends Repository<Company> {
-  async getByName(name: string) {
-    return this.findOne( name );
-  }
-}
--- my-typeorm-demo-server/src/company/person.repository.ts
+++
@@ -1,9 +0,0 @@
-import { Person } from './person.entity';
-import { EntityRepository, Repository } from 'typeorm';
-
-@EntityRepository(Person)
-export class PersonRepository extends Repository<Person> {
-  async getByName(name: string) {
-    return this.findOne( name );
-  }
-}
