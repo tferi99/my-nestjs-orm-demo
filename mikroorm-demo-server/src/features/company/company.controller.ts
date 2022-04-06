c16923a1e81e7efe2171f9d42e26feb8252b3d4a
ftoth <toth_ferenc@hotmail.com>
2022-04-06 23:46:50 +0200

    .

--- mikroorm-demo-server/src/features/company/company.controller.ts
+++ mikroorm-demo-server/src/features/company/company.controller.ts
@@ -2,13 +2,12 @@ import { Controller } from '@nestjs/common';
 import { Company } from './model/company.entity';
 import { CompanyRepository } from './company.repository';
 import { InjectRepository } from '@mikro-orm/nestjs';
-import { EventEmitterService } from '../../core/events/event-emitter.service';
 import { OrmCrudControllerBase } from '../../core/orm/controller/orm-crud-controller.base';
 import { CrudEntityRepository } from 'src/core/orm/service/crud-entity-repository';
 
 @Controller('company')
 export class CompanyController extends OrmCrudControllerBase<Company> {
-  constructor(@InjectRepository(Company) private companyRepository: CompanyRepository, private eventEmitterService: EventEmitterService) {
+  constructor(@InjectRepository(Company) private companyRepository: CompanyRepository) {
     super({ repository: companyRepository, getAllOptions: { orderBy: { name: 'ASC' } } });
   }
 
