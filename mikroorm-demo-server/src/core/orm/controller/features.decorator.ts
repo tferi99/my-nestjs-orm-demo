a8baa64959c51c1ab6babbc2c739d2f1b3ba1f2f
tferi99 <toth_ferenc@hotmail.com>
2022-07-03 22:11:30 +0100

    auto push by sync-git scripts

--- mikroorm-demo-server/src/core/orm/controller/features.decorator.ts
+++ mikroorm-demo-server/src/core/orm/controller/features.decorator.ts
@@ -2,4 +2,4 @@ import { SetMetadata } from '@nestjs/common';
 import { DEFAULT_ORM_CRUD_CONTROLLER_FEATURE_POLICY, EnabledFeatures } from './orm-crud-controller-feature.guard';
 
 export const ORM_CRUD_CONTROLLER_FEATURES_KEY = 'features';
-export const Features = (features: Partial<EnabledFeatures>) => SetMetadata(ORM_CRUD_CONTROLLER_FEATURES_KEY, {...DEFAULT_ORM_CRUD_CONTROLLER_FEATURE_POLICY, ...features});
+export const Features = (features: Partial<EnabledFeatures>) => SetMetadata(ORM_CRUD_CONTROLLER_FEATURES_KEY, { ...DEFAULT_ORM_CRUD_CONTROLLER_FEATURE_POLICY, ...features });
--- mikroorm-demo-server/src/core/orm/controller/orm-crud-controller-feature.guard.ts
+++ mikroorm-demo-server/src/core/orm/controller/orm-crud-controller-feature.guard.ts
@@ -4,57 +4,56 @@ import { OVERRIDE_GLOBAL_GUARD_KEY } from '../../../auth/passport/override-globa
 import { ORM_CRUD_CONTROLLER_FEATURES_KEY } from './features.decorator';
 
 export interface EnabledFeatures {
-    get: boolean;
-    getAll: boolean;
-    getAllFiltered: boolean;
-    insert: boolean;
-    update: boolean;
-    nativeUpdate: boolean;
-    delete: boolean;
-    nativeDelete: boolean;
-    nativeDeleteAll: boolean;
+  get: boolean;
+  getAll: boolean;
+  getAllFiltered: boolean;
+  insert: boolean;
+  update: boolean;
+  nativeUpdate: boolean;
+  delete: boolean;
+  nativeDelete: boolean;
+  nativeDeleteAll: boolean;
 }
 
 const OptimisticFeaturePolicy: EnabledFeatures = {
-    get: true,
-    getAll: true,
-    getAllFiltered: true,
-    insert: true,
-    update: false,
-    nativeUpdate: true,
-    delete: true,
-    nativeDelete: true,
-    nativeDeleteAll: true,
+  get: true,
+  getAll: true,
+  getAllFiltered: true,
+  insert: true,
+  update: false,
+  nativeUpdate: true,
+  delete: true,
+  nativeDelete: true,
+  nativeDeleteAll: true,
 };
 
 const PessimisticFeaturePolicy: EnabledFeatures = {
-    get: false,
-    getAll: false,
-    getAllFiltered: false,
-    insert: false,
-    update: false,
-    nativeUpdate: false,
-    delete: false,
-    nativeDelete: false,
-    nativeDeleteAll: false,
+  get: false,
+  getAll: false,
+  getAllFiltered: false,
+  insert: false,
+  update: false,
+  nativeUpdate: false,
+  delete: false,
+  nativeDelete: false,
+  nativeDeleteAll: false,
 };
 
 export const DEFAULT_ORM_CRUD_CONTROLLER_FEATURE_POLICY = PessimisticFeaturePolicy;
 
 @Injectable()
 export class OrmCrudControllerFeatureGuard implements CanActivate {
-    constructor(private reflector: Reflector) {
-    }
+  constructor(private reflector: Reflector) {}
 
-    canActivate(context: ExecutionContext): boolean {
-        console.log('--> OrmCrudControllerFeatureGuard.canActivate()');
+  canActivate(context: ExecutionContext): boolean {
+    console.log('--> OrmCrudControllerFeatureGuard.canActivate()');
 
-        const features: EnabledFeatures = this.reflector.getAllAndOverride<EnabledFeatures>(ORM_CRUD_CONTROLLER_FEATURES_KEY, [context.getClass()]);
-        console.log('FEATURES:', features);
-        const handler = context.getHandler();
-        //console.log('CTX:', context.getHandler().name, handler.arguments);
-        console.log('CTX:', context.getHandler().name);
+    const features: EnabledFeatures = this.reflector.getAllAndOverride<EnabledFeatures>(ORM_CRUD_CONTROLLER_FEATURES_KEY, [context.getClass()]);
+    console.log('FEATURES:', features);
+    const handler = context.getHandler();
+    //console.log('CTX:', context.getHandler().name, handler.arguments);
+    console.log('CTX:', context.getHandler().name);
 
-        return true;
-    }
+    return true;
+  }
 }
